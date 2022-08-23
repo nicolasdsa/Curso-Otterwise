7c05351828b382f1efa5c7a428145b77360bc433
Nicolas Sosa Machado <nicolassosamachado25@gmail.com>
2022-08-22 23:51:25 -0300

    Semana 5 Terminada

--- Semana-5/Exercicio-4-Estudo-de-Caso/index.js
+++ Semana-5/Exercicio-4-Estudo-de-Caso/index.js
@@ -1,6 +1,51 @@
 import { input } from "console-input";
+import fs from "fs";
 import axios from "axios";
 
-const userInput = input('Enter Your Name: ');
+const cepBusca = () => input("Digite o CEP que esta buscando:");
 
-console.log(userInput);  
+const json = (arquivo, data) => {
+  const toString = JSON.stringify(data);
+  fs.writeFileSync(arquivo, toString)};
+
+const search = async (cep) => {
+  try {
+    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+    return response.data;
+
+  } catch (error) {
+    console.log(error);
+
+  }
+};
+
+const principal = async () => {
+  try {
+    const cep = cepBusca();
+    const endereco = await search(cep);
+    json("endereco.json", endereco);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const randomCep = () => {
+  let randomCeps = []
+  for(let i = 0; i < 20; i++){
+
+    randomCeps.push(Math.floor(Math.random() * (29099999 - 29000000 ) + 29000000));
+  }
+  return randomCeps
+}
+const listCeps = randomCep();
+
+console.log(listCeps);
+
+principal();
+
+
+
+
+
+
+
